b90fd16e2b0c10473467ac4607181fa85b7662a2
khalidfawzi55 <162838229+khalidfawzi55@users.noreply.github.com>
2024-03-18 21:21:13 +0200

    Update category.controller.js

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -4,17 +4,7 @@ import { catchError } from "../../utlis/CatchErorr.js"
 
 const addCategory=addOne(categoryModel)
 
-const getAllCategories= catchError(async(req,res)=>{
-    const page_limit=2
-    const page_number=req.query.page*1||1
-    const skip=(page_number-1)*page_limit
-    let filter={}
-    if(req.params.categoryId){
-        filter={category:req.params.categoryId}
-    }
-        let result=await categoryModel.find(filter).skip(skip).limit(page_limit)
-         res.status(201).json({page:page_number,message:"success",result})
-     })
+const getAllCategories= getAllDocumnts(categoryModel)
     
 const updateCategory=updateOne(categoryModel);
 const deleteCategory=deleteOne(categoryModel);
